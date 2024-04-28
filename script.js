6f16659629b011dc67991492eec86888b5af4161
jjkhasbutcu <168339155+jjkhasbutcu@users.noreply.github.com>
2024-04-28 13:22:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,5 +10,14 @@ function addNote() {
     var list = document.getElementById("note-list");
     var listItem = document.createElement("li");
     listItem.textContent = text;
+
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "Sil";
+    deleteButton.className = "delete-button";
+    deleteButton.onclick = function() {
+        listItem.remove();
+    };
+
+    listItem.appendChild(deleteButton);
     list.appendChild(listItem);
 }
